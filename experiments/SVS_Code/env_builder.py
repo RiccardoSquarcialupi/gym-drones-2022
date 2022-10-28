544f55a84f7c731566ace8cc9eed6059e7f98519
Gyordan <jordancaminati@libero.it>
2022-10-28 21:46:36 +0200

    tsting

---
+++ experiments/SVS_Code/env_builder.py
@@ -0,0 +1,5 @@
+from gym_pybullet_drones.envs.multi_agent_rl.LeaderFollowerAviary import LeaderFollowerAviary
+
+
+class EnvBuilder():
+    pass
---
+++ experiments/SVS_Code/utils.py
@@ -0,0 +1,10 @@
+def from_env_name_to_class(env_name):
+    import importlib
+    module = importlib.import_module('gym_pybullet_drones.envs.multi_agent_rl.' + env_name)
+    env_class = getattr(module, env_name)
+    return env_class
+
+
+def build_env_by_name(env_class, **kwargs):
+    temp_env = env_class(**kwargs)
+    return lambda _: env_class(**kwargs), temp_env.observation_space, temp_env.action_space
