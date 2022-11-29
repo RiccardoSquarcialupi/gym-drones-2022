4497e445d8dccd7943998e43f3db13032da51205
RiccardoSquarcialupi <rikysquarcia@gmail.com>
2022-11-29 20:13:08 +0100

    add comment on build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -6,3 +6,6 @@ cd tests
 python test_build.py
 rm -rf results
 cd ..
+#require python 3.7-3.9
+# pip3 install numpy scipy Pillow matplotlib cycler gym pybullet torch 'ray[rllib]' stable-baseline3 scipy tensorboard
+# pip3 install --upgrade numpy scipy Pillow matplotlib cycler gym pybullet torch 'ray[rllib]' stable-baseline3 scipy tensorboard
