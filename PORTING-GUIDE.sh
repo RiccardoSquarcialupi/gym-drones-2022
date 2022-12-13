78da4ee01df30c479fcd314ce025c6180f3aa1e9
SVS-REMOTE-REPO <2022SVS.4@gmail.com>
2022-12-13 15:41:12 +0100

    update porting guide and pyproject toml files

--- PORTING-GUIDE.sh
+++ PORTING-GUIDE.sh
@@ -1,22 +1,18 @@
 #COMMAND for porting:
 	#create env
-	conda create -n battle-exp
+	conda create -n battle-exp python=3.7
+	#activate env
 	conda activate battle-exp
-	conda install python=3.7
-	#wait for conda to install everything(5/10 min required)
-	#activate env 	
-	#install all the required lib
-	conda install -c conda-forge ray-rllib libgcc poetry numpy pytorch pybullet
+	#install require lib, wait for conda to install everything(5/10 min required)
+	conda install -c conda-forge ray-rllib libgcc numpy pytorch pybullet
+	pip install poetry
 	#build the project
 	./build_project.sh
 	#upgrade some lib
-	#pip3 install --upgrade numpy Pillow matplotlib cycler
-	#pip3 install --upgrade gym pybullet stable_baselines3 'ray[rllib]'
-	pip install --upgrade numpy scipy Pillow matplotlib cycler gym pybullet torch pytorch 'ray[rllib]' stable-baseline3 tensorboard
+	pip install --upgrade numpy scipy Pillow matplotlib cycler gym pybullet torch 'ray[rllib]' stable-baselines3 tensorboard libgcc
 	#file "GLIBCXX_3.4.29 not found inside /usr/lib/x86_64 etc..., 
-	#the file libstdc++ of our linux distribution it's not update we install the library 	
-	#contains this file inside conda and export his /lib folder to LD_LIBRARY to bypass this problem 
+	#the file libstdc++ of our linux distribution it's not update
+	#we install the library that contain this file inside conda and export his /lib folder to LD_LIBRARY to bypass this problem 
 	export LD_LIBRARY=$LD_LIBRARY_PATH:/home/SVSstudentsX/anaconda3/lib
-	
 	#double check the python version with python --version
 	
